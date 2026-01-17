e0bb24ee6867f8970377a7bb3f07d16522eec951
Wes te Braak <wesbraak3@hotmail.com>
2026-01-17 11:50:48 +0100

    player movement decentralised

---
+++ Assets/Scripts/Player/BaseInput.cs
@@ -0,0 +1,22 @@
+﻿using PurrNet;
+using Unity.Cinemachine;
+using UnityEngine;
+
+public abstract class BaseInput : MonoBehaviour, IMoveInput, ILookDirection {
+    public abstract Vector2 MoveDirection { get; }
+    public abstract Vector2 MousePosition { get; }
+    public abstract bool IsLooking { get; }
+
+    //[SerializeField] private CinemachineCamera _playerCamera;
+
+    //protected override void OnSpawned() {
+    //    base.OnSpawned();
+
+    //    enabled = isOwner;
+
+    //    _playerCamera.Priority.Value = isOwner ? 10 : 0;
+    //    if (isOwner) {
+    //        _playerCamera.transform.SetParent(null);
+    //    }
+    //}
+}
